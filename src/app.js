49668452528b39697a7553de1fe0b36e5363a46b
Jin Kim <jinkim477@gmail.com>
2025-11-12 18:43:13 -0700

    added profile pic upload

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ import pool from "./config/db.js";
 import usersRouter from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import resourcesRouter from "./routes/resources.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/upload", uploadRouter);
 
 app.use("/api/users", usersRouter);
 app.use("/api/auth", authRoutes)
