8b3955347ea55bcf17aa8f34ef2be0a09c8d77db
Aidan <aidanhuang10@gmail.com>
2026-01-16 11:12:04 -0700

    feature: register discussions router in express app
    - Import discussionsRouter from routes/discussions.js
    - Mount discussions router at /api/discussions
    - Maintain route priority order for proper handling

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import uploadRouter from "./routes/uploads.js";
 import clubsRouter from "./routes/clubs.js";
 import postsRouter from "./routes/posts.js";
 import commentRouter from "./routes/comments.js";
+import discussionsRouter from "./routes/discussions.js";
 
 const app = express();
 app.use(cors());
@@ -17,6 +18,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/clubs", clubsRouter);
 app.use("/api/posts", postsRouter);
+app.use("/api/discussions", discussionsRouter);
 app.use("/api/comments", commentRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/resources", resourcesRouter);
